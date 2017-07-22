d4b316ce7b474e0caf651ae41075529f6e9a5104
prideless <prideless@pridelesss-MacBook-Air.local>
2017-07-21 23:58:48 -0400

    July updates

--- server.js
+++ server.js
@@ -2,17 +2,12 @@ var http         = require('http');
 var fs           = require('fs');
 var path         = require('path');
 
-// custom imports
 var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars');
 
-//openshift settings
-var server_port = process.env.PORT || 8080;
+var server_port = process.env.PORT || 8081;
 
-
-
-// custom settings
 app.set('views', __dirname + '/views');
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -20,6 +15,10 @@ app.use('/', express.static(__dirname + '/public'));
 
 app.get('/', function(req,res){
     res.render('index.handlebars');
+});
+
+app.get('/photos', function(req,res){
+    res.render('photos.handlebars');
 });
 
 app.get('/bobBooks', function(req, res){
